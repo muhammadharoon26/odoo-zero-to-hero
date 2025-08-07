561037f83f0cae79ab33c1556cd1b86cc6e0f85b
Muhammad Haroon <mharoon1326@gmail.com>
2025-08-07 12:52:42 +0500

    init

--- __manifest__.py
+++ __manifest__.py
@@ -2,11 +2,14 @@
     "name": "Library Management",
     "version": "1.0",
     "summary": "Manage books in library",
+    "license": "LGPL-3",
+    'sequence':'2',
     "depends": ["base"],
     "data": [
         "security/ir.model.access.csv",
         "views/book_views.xml",
     ],
     "installable": True,
-    "auto_install": True,
+    "auto_install": False,
+    "application": True,
 }
